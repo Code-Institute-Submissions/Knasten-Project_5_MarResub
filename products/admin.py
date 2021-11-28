b3aad2597a9fe5b3c3cade9099cc650ee12e5277
Hampus Nilsson <hampusjojo@gmail.com>
2021-11-28 18:19:19 +0000

    add admin classes in products, add products images

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
 
-admin.site.register(Category)
-admin.site.register(Product)
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'category',
+        'make',
+        'model_id',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
