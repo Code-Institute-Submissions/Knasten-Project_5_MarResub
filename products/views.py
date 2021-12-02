dd0e4ff2fd3e2e9a62058967dbb84143bec4a695
Hampus Nilsson <hampusjojo@gmail.com>
2021-12-02 17:25:58 +0000

    add sorting, update allauth templates

--- products/views.py
+++ products/views.py
@@ -10,6 +10,8 @@ def all_products(request):
     products = Product.objects.all()
     categories = Category.objects.all()
     query = None  # Needed to make sure it dosent bug, when not entering a seach value.
+    sort = None
+    direction = None
 
     if request.GET:
         if 'search_bar' in request.GET:
@@ -20,10 +22,26 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort = f'-{sort}'
+                products = products.order_by(sort)
+
+    selected_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        'categories': categories,
+        'selected_categories': categories,
+        'selected_sorting': selected_sorting,
         'search_kw': query,
     }
     return render(request, 'products/products.html', context)
