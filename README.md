d354d842f58a770da0ca83759e00d4652742cc39
Hampus Nilsson <hampusjojo@gmail.com>
2021-12-05 16:09:01 +0000

    add toaster logic

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Problems occured: (1(Media not showing up )), (2(Some products imgs not working in cart))
-Solutions: ((1)change this line "[BASE_DIR / 'media']" for "os.path.join(BASE_DIR, 'media')"), (2(Spelling mistake on a url link instead of "." I had "_"))
+Problems occured: (1(Media not showing up )), (2(Some products imgs not working in cart)), (3(Problem with toast and/or collasible menus))
+Solutions: ((1)change this line "[BASE_DIR / 'media']" for "os.path.join(BASE_DIR, 'media')"), (2(Spelling mistake on a url link instead of "." I had "_")),
+(3(Had to import other versions of scripts and remove "bs" from collasible menus, example "data-bs-toggle" is now "data-toggle" This is beacuse I now import bootstrap 4 instead of 5 and they have changed the code for this.))
 
 Other:
 # https://django-allauth.readthedocs.io/en/latest/faq.html?highlight=EMAIL_BACKEND#when-i-sign-up-i-run-into-connectivity-errors-connection-refused-et-al
--- cart/views.py
+++ cart/views.py
@@ -13,13 +13,14 @@ def view_cart(request):
 
 def add_to_cart(request, product_id):
 
-    product = Product.objects.get(pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if product_id in list(cart.keys()):
         cart[product_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}')
     else:
         cart[product_id] = quantity
         messages.success(request, f'Added {product.name} to your cart!')
@@ -30,13 +31,16 @@ def add_to_cart(request, product_id):
 
 def change_cart(request, product_id):
 
+    product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[product_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[product_id]}!')
     else:
         cart.pop(product_id)
+        messages.success(request, f'Removed {product.name} from cart!')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -48,10 +52,11 @@ def remove_from_cart(request, product_id):
         product = get_object_or_404(Product, pk=product_id)
         cart = request.session.get('cart', {})
         cart.pop(product_id)
+        messages.success(request, f'Removed {product.name} from cart!')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
-        print(e)
+        messages.error(request, f'Error: {e} \n Item not removed!')
         return HttpResponse(status=500)
