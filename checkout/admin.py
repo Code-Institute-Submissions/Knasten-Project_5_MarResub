95a5450943d9ea8dd61edb458be3faa14930950a
Hampus Nilsson <hampusjojo@gmail.com>
2021-12-07 02:45:33 +0000

    finish webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,11 +12,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                         'delivery_cost', 'order_total',
-                        'grand_total',)
+                        'grand_total', 'original_cart',
+                        'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total','original_cart',
+                    'stripe_pid',)
 
     ordering = ('-date',)
 
