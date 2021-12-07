9e83cda0f4414464d54e8eaf18e93af53a6e4a47
Hampus Nilsson <hampusjojo@gmail.com>
2021-12-07 16:04:05 +0000

    addremove and edit links, add remove logic

--- products/views.py
+++ products/views.py
@@ -65,9 +65,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product was added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Something went wrong! Please check the form and try again')
     form = ProductForm()
@@ -101,3 +101,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """View to delete product"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product was successfully deleted')
+    return(redirect(reverse('products')))
