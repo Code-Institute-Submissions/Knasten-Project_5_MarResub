91f775284062d5f487115fa81575fe4b8b1e46e8
Hampus Nilsson <hampusjojo@gmail.com>
2022-02-24 19:59:34 +0000

    add testimonial model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
+class Testimonials(models.Model):
+    choices = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    )
+
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="testimonials")
+    created_on = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(5)], default=1, choices=choices)
+    content = models.TextField(max_length=1000)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.name} rated {self.rating}'
