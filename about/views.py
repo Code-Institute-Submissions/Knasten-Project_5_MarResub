6627ee7274c4a3dc44fc82af5ddd30503744ac12
Hampus Nilsson <hampusjojo@gmail.com>
2022-02-26 22:15:09 +0000

    add autofill of username on testimonial form

--- about/views.py
+++ about/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
 from .forms import TestimonialForm
 from .models import Testimonial
 
 # Create your views here.
-
+    
 
 def about_view(request):
+    if request.method == "POST":
+        form = TestimonialForm(request.POST, request.FILES)
+        if form.is_valid():
+            testimonial = form.save(commit=False)
+            testimonial.name = request.user
+            testimonial.save()
+            messages.success(request, 'Testimonial has been submitted for review!')
+            return redirect('about')
     form = TestimonialForm()
     template = 'about.html'
 
