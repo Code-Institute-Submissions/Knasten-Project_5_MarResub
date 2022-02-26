5bbe615ee41c5618b4af3a4e0b08a0430c8c6ca7
Hampus Nilsson <hampusjojo@gmail.com>
2022-02-26 21:21:44 +0000

    add about view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import TestimonialForm
+from .models import Testimonial
 
 # Create your views here.
+
+
+def about_view(request):
+    form = TestimonialForm()
+    template = 'about.html'
+
+    testimonials = Testimonial.objects.filter(approved=True)
+
+    context = {
+        'form': form,
+        'testimonials': testimonials,
+    }
+
+    return render(request, template, context)
