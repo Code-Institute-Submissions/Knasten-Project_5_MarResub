d53c6581dc48ee6bbc6d8608dfc6c6b490f1f1b6
Hampus Nilsson <hampusjojo@gmail.com>
2022-03-01 06:45:39 +0000

    add Question

--- about/models.py
+++ about/models.py
@@ -25,3 +25,18 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return f'{self.name} submitted a testimonial with rating of: {self.rating}'
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True)
+    title = models.CharField(max_length=50)
+    question = models.TextField(max_length=5000)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.name} asked a question on {self.created_on}"
