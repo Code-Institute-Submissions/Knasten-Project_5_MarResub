364702271f52de8ba55c5134c7454463a75caee8
Hampus Nilsson <hampusjojo@gmail.com>
2022-03-02 04:03:46 +0000

    add review model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -33,3 +34,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="reviews", on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, related_name="user", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    review = models.TextField(max_length=1000)
+    created_on = models.DateField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
